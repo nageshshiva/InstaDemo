4bf8ebf5869cfefbdc5d6c5a44e1ab906feb4cc4
nageshshiva <shivanagesh.6@gmail.com>
2017-08-09 12:50:28 +0530

    class code added

--- app/src/main/java/com/example/snc/instademo/MainActivity.java
+++ app/src/main/java/com/example/snc/instademo/MainActivity.java
@@ -1,28 +1,43 @@
 package com.example.snc.instademo;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ImageView;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private ProgressDialog mProgressDialog;
     FirebaseStorage storage;
     StorageReference mStorageRef;
+    FirebaseAuth firebaseAuth;
+    DatabaseReference mDatabase;
 
     static final int GALLERY_REQUEST = 10;
     Button addBTN;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +45,16 @@ public class MainActivity extends AppCompatActivity {
 
         addBTN = (Button)findViewById(R.id.upload_btn);
         storage = FirebaseStorage.getInstance();
-        mStorageRef = storage.getReference();
+        mStorageRef = storage.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("allPosts");
+        firebaseAuth = FirebaseAuth.getInstance();
 
+        recyclerView = (RecyclerView) findViewById(R.id.main_recyclerview_home);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(MainActivity.this,3);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        mProgressDialog = new ProgressDialog(this);
+        mProgressDialog.setMessage("Sharing.....");
 
         addBTN.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,26 +72,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == GALLERY_REQUEST){
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
 
-            if (resultCode == RESULT_OK){
-
-                Uri filepath = data.getData();
-                mStorageRef.child("photos").child(filepath.getLastPathSegment())
-                .putFile(filepath).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        Toast.makeText(MainActivity.this, "image uploaded", Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-            else{
-
-                Toast.makeText(this, "Error..", Toast.LENGTH_SHORT).show();
-            }
+                startPosting(data);
         }
     }
 
+    private void startPosting(Intent data){
+
+        Uri filepath = data.getData();
+        mStorageRef.child("photos").child(filepath.getLastPathSegment())
+                .putFile(filepath).addOnSuccessListener(this, new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                Uri downloaduri = taskSnapshot.getDownloadUrl();
+                DatabaseReference newpost = mDatabase.push();
+                newpost.child("image").setValue(downloaduri.toString());
+                newpost.child("uid").setValue(firebaseAuth.getCurrentUser().getUid());
+            }
+        });
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        FirebaseRecyclerAdapter<Post, PostViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Post, PostViewHolder>(Post.class, R.layout.row, PostViewHolder.class, mDatabase) {
+            @Override
+            protected void populateViewHolder(PostViewHolder viewHolder, Post model, int position) {
+                viewHolder.setImage(getApplicationContext(), model.getImage());
+            }
+        };
+
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    public static  class PostViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+        public PostViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+
+        }
+
+        public void setImage(Context context, String image)
+        {
+            ImageView post_img = (ImageView) mView.findViewById(R.id.card_post_imageview);
+            Picasso.with(context).load(image).resize(100,100).centerCrop().into(post_img);
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menumyaccount, menu);
---
+++ app/src/main/java/com/example/snc/instademo/Post.java
@@ -0,0 +1,27 @@
+package com.example.snc.instademo;
+
+/**
+ * Created by Shiva on 8/8/2017.
+ */
+
+public class Post  {
+
+    private String image;
+    private String Uid;
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getUid() {
+        return Uid;
+    }
+
+    public void setUid(String uid) {
+        Uid = uid;
+    }
+}

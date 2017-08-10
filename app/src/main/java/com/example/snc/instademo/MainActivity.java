6f4dda399827e667d59bf55dc7a361f4b08ec1c0
nageshshiva <shivanagesh.6@gmail.comcls>
2017-08-10 12:12:28 +0530

    class code added 10-08-2017

--- app/src/main/java/com/example/snc/instademo/MainActivity.java
+++ app/src/main/java/com/example/snc/instademo/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
         mProgressDialog = new ProgressDialog(this);
         mProgressDialog.setMessage("Sharing.....");
 
+        //button added
         addBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
